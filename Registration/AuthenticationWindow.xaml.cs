40482ea24f396366c56ef1f2375ee697e3e1fe6b
Alex Balezin <alexbalezin27@yandex,ru>
2022-01-22 16:08:00 +0300

    creating project

---
+++ Registration/AuthenticationWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace Registration
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthenticationWindow.xaml
+    /// </summary>
+    public partial class AuthenticationWindow : Window
+    {
+        ApplicationContext db;
+        public AuthenticationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private bool Check(string login, string password)
+        {
+            User currentUser = null;
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                currentUser = db.Users.Where(x => x.Login == login && x.Pass == password).FirstOrDefault();
+                if (currentUser != null)
+                    return true;
+                else
+                    return false;
+            }
+
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e) 
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Hide();
+        }
+
+        private void Button_Auth_Click(object sender, RoutedEventArgs e)
+        {
+            string login = loginField.Text.Trim();
+            string password = passwordField.Password.Trim();
+
+            if (Check(login, password))
+            {
+                passwordField.ToolTip = "";
+                passwordField.Background = Brushes.Transparent;
+                loginField.ToolTip = "";
+                loginField.Background = Brushes.Transparent;
+                var notification = new ToastContentBuilder();
+                notification.AddText("Вход выполнен");
+                notification.Show();
+            }
+            else
+            {
+                var notification = new ToastContentBuilder();
+                notification.AddText("Неверный логин или пароль");
+                notification.Show();
+            }
+
+        }
+    }
+}
---
+++ Registration/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Registration
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ApplicationContext db;
+        public MainWindow()
+        {
+            InitializeComponent();
+            GreetingText.Text = ResourceText.Greeting;
+            db = new ApplicationContext();
+
+        }
+
+        private void Registration_Click(object sender, RoutedEventArgs e)
+        {
+            string login = loginField.Text.Trim();
+            string password = passwordField.Password.Trim();
+            string passwordRepeat = repeatPasswordField.Password.Trim();
+            string email = emailField.Text.Trim().ToLower();
+
+            if (Check(login, email, password, passwordRepeat))
+            {
+                if (!db.Users.Any(x => x.Login == login))
+                {
+                    User user = new User(login, email, password);
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                }
+            }
+
+        }
+
+        private void Enter_Button_Click(object sender, RoutedEventArgs e)
+        {
+            AuthenticationWindow authenticationWindow = new AuthenticationWindow();
+            authenticationWindow.Show();
+            Hide();
+
+        }
+        private bool Check(string login, string email, string password, string passwordRepeat)
+        {
+            if (login.Length < 5)
+            {
+                loginField.ToolTip = "Логин должен содержать больше 5 символов";
+                loginField.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                loginField.ToolTip = "";
+                loginField.Background = Brushes.Transparent;
+            }
+
+            if (!email.Contains("@"))
+            {
+                emailField.ToolTip = "Email должен содержать" + "\"" + "@" + "\"";
+                emailField.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                emailField.ToolTip = "";
+                emailField.Background = Brushes.Transparent;
+            }
+
+            if (password.Length < 5)
+            {
+                passwordField.ToolTip = "Пароль должен содержать больше 5 символов";
+                passwordField.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                passwordField.ToolTip = "";
+                passwordField.Background = Brushes.Transparent;
+            }
+
+            if (password != passwordRepeat)
+            {
+                repeatPasswordField.ToolTip = "Введенные пароли должны совпадать";
+                repeatPasswordField.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                repeatPasswordField.ToolTip = "";
+                repeatPasswordField.Background = Brushes.Transparent;
+            }
+
+            if (loginField.Background != Brushes.DarkRed &&
+                 emailField.Background != Brushes.DarkRed &&
+                 passwordField.Background != Brushes.DarkRed &&
+                 repeatPasswordField.Background != Brushes.DarkRed
+                 )
+                return true;
+            else
+                return false;
+
+        }
+    }
+}
